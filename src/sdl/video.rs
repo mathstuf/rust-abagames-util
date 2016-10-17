f69aea10d6fde10cc04527cf543ba44bacf6dd7a
Ben Boeckel <mathstuf@gmail.com>
2016-10-16 22:09:27 -0400

    video: add a method for the main drawing infrastructure

--- src/sdl/video.rs
+++ src/sdl/video.rs
@@ -1,9 +1,22 @@
 // Distributed under the OSI-approved BSD 2-Clause License.
 // See accompanying file LICENSE for details.
 
+extern crate draw_state;
+use self::draw_state::target::Rect;
+
+extern crate gfx;
+use self::gfx::Encoder;
+
+extern crate gfx_core;
+use self::gfx_core::Device;
+use self::gfx_core::draw::CommandBuffer;
+use self::gfx_core::format::{DepthStencil, Srgba8};
+use self::gfx_core::handle::{DepthStencilView, RenderTargetView};
+
 extern crate gfx_device_gl;
 use self::gfx_device_gl::Device as GLDevice;
-use self::gfx_device_gl::Factory;
+use self::gfx_device_gl::CommandBuffer as GLCommandBuffer;
+use self::gfx_device_gl::{Factory, Resources};
 
 extern crate gfx_window_sdl;
 
@@ -17,17 +30,22 @@ use self::sdl2::video::{GLContext, GLProfile, Window};
 
 use std::error::Error;
 
+pub type ObjectEncoder = Encoder<Resources, GLCommandBuffer>;
+
 pub struct Video {
     window: Window,
     _gl_context: GLContext,
     device: GLDevice,
     factory: Factory,
+    view: RenderTargetView<Resources, Srgba8>,
+    depth_stencil_view: DepthStencilView<Resources, DepthStencil>,
 
     perspective_matrix: PerspectiveMatrix3<f32>,
 }
 
 static NEAR_PLANE: f32 = 0.1;
 static FAR_PLANE: f32 = 1000f32;
+static CLEAR_COLOR: [f32; 4] = [0f32, 0f32, 0f32, 0f32];
 
 impl Video {
     pub fn new(sdl_context: &Sdl, caption: &str, size: &(u32, u32), windowed: bool) -> Result<Self, Box<Error>> {
@@ -51,7 +69,7 @@ impl Video {
             window.fullscreen_desktop();
         }
 
-        let (window, gl_context, device, factory, _, _) =
+        let (window, gl_context, device, factory, view, depth_stencil_view) =
             gfx_window_sdl::init(&mut window);
 
         let mut renderer = try!(window.renderer().build());
@@ -77,6 +95,8 @@ impl Video {
             _gl_context: gl_context,
             device: device,
             factory: factory,
+            view: view,
+            depth_stencil_view: depth_stencil_view,
         })
     }
 
@@ -90,5 +110,33 @@ impl Video {
 
     pub fn perspective_matrix(&self) -> &Matrix4<f32> {
         self.perspective_matrix.as_matrix()
+    }
+
+    pub fn render_with<F>(&mut self, mut render: F)
+        where F: FnMut(&mut ObjectEncoder),
+    {
+        let mut buffer = self.factory.create_command_buffer();
+
+        let size = self.window.size();
+        buffer.set_scissor(Rect {
+            x: 0,
+            y: 0,
+            w: size.0 as u16,
+            h: size.1 as u16,
+        });
+
+        let mut encoder: ObjectEncoder = self.factory
+            .create_command_buffer()
+            .into();
+
+        encoder.clear(&mut self.view, CLEAR_COLOR);
+        encoder.clear_depth(&mut self.depth_stencil_view, 0f32);
+        encoder.clear_stencil(&mut self.depth_stencil_view, 0);
+
+        render(&mut encoder);
+
+        encoder.flush(&mut self.device);
+        self.window.gl_swap_window();
+        self.device.cleanup();
     }
 }
