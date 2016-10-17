719a5b5b3b0d3484e5b7e7be0cb05dd48dfea5e5
Ben Boeckel <mathstuf@gmail.com>
2016-10-16 21:21:13 -0400

    audio: load music and sfx

--- src/sdl/audio.rs
+++ src/sdl/audio.rs
@@ -5,19 +5,55 @@ extern crate sdl2;
 use self::sdl2::Sdl;
 
 extern crate sdl2_mixer;
-use self::sdl2_mixer::{AudioFormat, Sdl2MixerContext};
+use self::sdl2_mixer::{AudioFormat, Channel, Chunk, Music, Sdl2MixerContext};
 
+use super::super::paths::Paths;
+
+use std::collections::hash_map::HashMap;
 use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
 
 pub struct AudioData {
     _sdl_mixer_context: Sdl2MixerContext,
+    path: PathBuf,
+
+    music: HashMap<String, Music>,
+
+    sfx: HashMap<String, (Chunk, Channel)>,
 }
 
 impl AudioData {
-    fn new(context: Sdl2MixerContext) -> Result<Self, Box<Error>> {
+    fn new(context: Sdl2MixerContext, asset_dir: PathBuf) -> Result<Self, Box<Error>> {
+        let sounds_dir = asset_dir.join("sounds");
+
+        let read_dir = try!(fs::read_dir(sounds_dir.join("musics")));
+        let music = try!(read_dir.map(|entry| {
+            let entry = try!(entry);
+            let music = try!(Music::from_file(&entry.path()));
+            let file_name = entry.file_name().to_string_lossy().into_owned();
+
+            Ok((file_name, music))
+        })
+        .collect::<Result<HashMap<_, _>, Box<Error>>>());
+
         Ok(AudioData {
             _sdl_mixer_context: context,
+            path: sounds_dir,
+
+            music: music,
+
+            sfx: HashMap::new(),
         })
+    }
+
+    fn load_sfx(&mut self, name: &str, channel: isize) -> Result<(), Box<Error>> {
+        let path = self.path.join("chunks").join(name);
+        let chunk = try!(Chunk::from_file(&path));
+
+        self.sfx.insert(name.to_string(), (chunk, sdl2_mixer::channel(channel)));
+
+        Ok(())
     }
 }
 
@@ -43,8 +79,21 @@ impl Audio {
         try!(sdl2_mixer::open_audio(FREQUENCY, FORMAT, CHANNELS, BUFFERS));
         sdl2_mixer::allocate_channels(CHANNELS);
 
-        let data = try!(AudioData::new(context));
+        let asset_dir = try!(Paths::new()).asset_dir;
+        let data = try!(AudioData::new(context, asset_dir));
 
         Ok(Audio::Enabled(data))
+    }
+
+    pub fn load_sfx(&mut self, sfx: &[(&str, isize)]) -> Result<&mut Self, Box<Error>> {
+        if let Audio::Enabled(ref mut data) = *self {
+            try!(sfx.iter()
+                .map(|&(ref name, channel)| {
+                    data.load_sfx(name, channel)
+                })
+                .collect::<Result<Vec<_>, _>>());
+        }
+
+        Ok(self)
     }
 }
