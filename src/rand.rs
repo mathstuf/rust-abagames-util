40f9c022efe242072ffdc044ac06f81f8d6714b0
Ben Boeckel <mathstuf@gmail.com>
2016-10-22 22:36:49 -0400

    rand: remove refcell
    Just make the methods mutable

--- src/rand.rs
+++ src/rand.rs
@@ -10,10 +10,8 @@ use self::mersenne_twister::MT19937;
 extern crate rand;
 use self::rand::{Rng, SeedableRng};
 
-use std::cell::RefCell;
-
 pub struct Rand {
-    twister: RefCell<MT19937>,
+    twister: MT19937,
 }
 
 impl Rand {
@@ -21,39 +19,39 @@ impl Rand {
         let seed = UTC::now().timestamp() as u32;
 
         Rand {
-            twister: RefCell::new(MT19937::from_seed(seed)),
+            twister: MT19937::from_seed(seed),
         }
     }
 
-    pub fn set_seed(&self, seed: u32) {
-        self.twister.borrow_mut().reseed(seed)
+    pub fn set_seed(&mut self, seed: u32) {
+        self.twister.reseed(seed)
     }
 
-    pub fn next_u32(&self) -> u32 {
-        self.twister.borrow_mut().next_u32()
+    pub fn next_u32(&mut self) -> u32 {
+        self.twister.next_u32()
     }
 
-    fn next_i32(&self) -> i32 {
+    fn next_i32(&mut self) -> i32 {
         self.next_u32() as i32
     }
 
-    pub fn next_int(&self, n: u32) -> u32 {
+    pub fn next_int(&mut self, n: u32) -> u32 {
         self.next_u32() % n
     }
 
-    pub fn next_int_signed(&self, n: i32) -> i32 {
+    pub fn next_int_signed(&mut self, n: i32) -> i32 {
         self.next_i32() % (2 * n + 1) - n
     }
 
-    fn next_real(&self) -> f32 {
+    fn next_real(&mut self) -> f32 {
         ((self.next_u32() as f64) * (1. / 4294967295.)) as f32
     }
 
-    pub fn next_float(&self, n: f32) -> f32 {
+    pub fn next_float(&mut self, n: f32) -> f32 {
         self.next_real() % n
     }
 
-    pub fn next_float_signed(&self, n: f32) -> f32 {
+    pub fn next_float_signed(&mut self, n: f32) -> f32 {
         self.next_real() % (2. * n) - n
     }
 }
