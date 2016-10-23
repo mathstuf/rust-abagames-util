ac86667a4543a22e64e34c63e69346594d2c3766
Ben Boeckel <mathstuf@gmail.com>
2016-10-22 22:37:44 -0400

    rand: test that it is deterministic

--- src/rand.rs
+++ src/rand.rs
@@ -55,3 +55,42 @@ impl Rand {
         self.next_real() % (2. * n) - n
     }
 }
+
+#[cfg(test)]
+mod test {
+    extern crate itertools;
+
+    use super::Rand;
+
+    fn run_rand<T, F>(closure: F) -> Vec<T>
+        where F: FnMut() -> T,
+    {
+        itertools::repeat_call(closure)
+            .take(20)
+            .collect()
+    }
+
+    #[test]
+    fn test_seed_is_deterministic() {
+        let mut rand_0 = Rand::new();
+        let mut rand_1 = Rand::new();
+
+        rand_0.set_seed(1);
+        rand_1.set_seed(1);
+
+        assert_eq!(run_rand(|| rand_0.next_u32()),
+                   run_rand(|| rand_1.next_u32()));
+        assert_eq!(run_rand(|| rand_0.next_int(10)),
+                   run_rand(|| rand_1.next_int(10)));
+        assert_eq!(run_rand(|| rand_0.next_int(200)),
+                   run_rand(|| rand_1.next_int(200)));
+        assert_eq!(run_rand(|| rand_0.next_int_signed(200)),
+                   run_rand(|| rand_1.next_int_signed(200)));
+        assert_eq!(run_rand(|| rand_0.next_real()),
+                   run_rand(|| rand_1.next_real()));
+        assert_eq!(run_rand(|| rand_0.next_float(0.2)),
+                   run_rand(|| rand_1.next_float(0.2)));
+        assert_eq!(run_rand(|| rand_0.next_float_signed(-0.4)),
+                   run_rand(|| rand_1.next_float_signed(-0.4)));
+    }
+}
