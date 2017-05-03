3295b8f9a7c32b70537d4fe64a0d17bf86f6043c
Ben Boeckel <mathstuf@gmail.com>
2017-05-02 21:05:23 -0400

    slice: support making slices with index arrays

--- src/slice.rs
+++ src/slice.rs
@@ -24,6 +24,26 @@ pub fn slice_for_loop<R, F>(factory: &mut F, size: u32) -> gfx::Slice<R>
     }
 }
 
+/// Compute a slice for a line loop of the given size based on a list of indices.
+pub fn slice_for_loop_with<R, F>(factory: &mut F, data: &[u16]) -> gfx::Slice<R>
+    where R: gfx::Resources,
+          F: gfx::Factory<R>,
+{
+    let first = data[0];
+    let indices = data.into_iter()
+        .chain(iter::once(&first))
+        .cloned()
+        .collect::<Vec<u16>>();
+
+    gfx::Slice {
+        start: 0,
+        end: indices.len() as u32,
+        base_vertex: 0,
+        instances: None,
+        buffer: indices.into_index_buffer(factory),
+    }
+}
+
 /// Compute a slice for a fan consisting of a number of triangles.
 pub fn slice_for_fan<R, F>(factory: &mut F, size: u32) -> gfx::Slice<R>
     where R: gfx::Resources,
@@ -35,6 +55,29 @@ pub fn slice_for_fan<R, F>(factory: &mut F, size: u32) -> gfx::Slice<R>
         indices.push(0);
         indices.push(i as u16);
         indices.push(j as u16);
+    }
+
+    gfx::Slice {
+        start: 0,
+        end: indices.len() as u32,
+        base_vertex: 0,
+        instances: None,
+        buffer: indices.into_index_buffer(factory),
+    }
+}
+
+/// Compute a slice for a fan consisting of a number of triangles based on a list of indices.
+pub fn slice_for_fan_with<R, F>(factory: &mut F, data: &[u16]) -> gfx::Slice<R>
+    where R: gfx::Resources,
+          F: gfx::Factory<R>,
+{
+    let size = data.len();
+    let mut indices: Vec<u16> = Vec::with_capacity(size * 2 - 2);
+
+    for (i, j) in (1..size).zip(2..size) {
+        indices.push(data[0]);
+        indices.push(data[i]);
+        indices.push(data[j]);
     }
 
     gfx::Slice {
