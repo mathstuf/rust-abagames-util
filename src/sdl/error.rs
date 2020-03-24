279683c946be43e431139972c01478f11474f8e9
Ben Boeckel <mathstuf@gmail.com>
2020-03-24 19:38:31 -0400

    rust: add `dyn` keyword

--- src/sdl/error.rs
+++ src/sdl/error.rs
@@ -141,7 +141,7 @@ pub struct Error {
 pub type Result<T> = StdResult<T, Error>;
 
 impl Fail for Error {
-    fn cause(&self) -> Option<&Fail> {
+    fn cause(&self) -> Option<&dyn Fail> {
         self.inner.cause()
     }
 
