5166cd286492a1ee7a2f00a95a5949a74febdd68
Ben Boeckel <mathstuf@gmail.com>
2021-07-03 16:54:47 -0400

    clippy: simplify `.inspect().for_each()` call chains

--- src/rand.rs
+++ src/rand.rs
@@ -108,46 +108,38 @@ mod test {
         println!("seed: {}", seed);
         rand.set_seed(seed);
 
-        (0..100)
-            .into_iter()
-            .inspect(|n| println!("\nrand.next_int({:?})...", n))
-            .for_each(|n| assert!(verify_rand(|| rand.next_int(n), |i| i < n || n == 0)));
-        (0..100)
-            .into_iter()
-            .inspect(|n| println!("\nrand.next_int_signed({:?})...", n))
-            .for_each(|n| {
-                assert!(verify_rand(
-                    || rand.next_int_signed(n),
-                    |i| {
-                        let n = n as i32;
-                        -n <= i && i <= n
-                    }
-                ))
-            });
-        (0..100)
-            .into_iter()
-            .inspect(|_| println!("\nrand.next_real()..."))
-            .for_each(|_| assert!(verify_rand(|| rand.next_real(), |f| 0. <= f && f < 1.)));
-        (0..100)
-            .into_iter()
-            .map(|n: usize| n as f32)
-            .inspect(|n| println!("\nrand.next_float({:?})...", n))
-            .for_each(|n| {
-                assert!(verify_rand(
-                    || rand.next_float(n),
-                    |f| (0. <= f && f < n) || n == 0.
-                ))
-            });
-        (0..100)
-            .into_iter()
-            .map(|n: usize| n as f32)
-            .inspect(|n| println!("\nrand.next_float_signed({:?})...", n))
-            .for_each(|n| {
-                assert!(verify_rand(
-                    || rand.next_float_signed(n),
-                    |f| (-n <= f && f < n) || n == 0.
-                ))
-            });
+        (0..100).into_iter().for_each(|n| {
+            println!("\nrand.next_int({:?})...", n);
+            assert!(verify_rand(|| rand.next_int(n), |i| i < n || n == 0))
+        });
+        (0..100).into_iter().for_each(|n| {
+            println!("\nrand.next_int_signed({:?})...", n);
+            assert!(verify_rand(
+                || rand.next_int_signed(n),
+                |i| {
+                    let n = n as i32;
+                    -n <= i && i <= n
+                }
+            ))
+        });
+        (0..100).into_iter().for_each(|_| {
+            println!("\nrand.next_real()...");
+            assert!(verify_rand(|| rand.next_real(), |f| 0. <= f && f < 1.))
+        });
+        (0..100).into_iter().map(|n: usize| n as f32).for_each(|n| {
+            println!("\nrand.next_float({:?})...", n);
+            assert!(verify_rand(
+                || rand.next_float(n),
+                |f| (0. <= f && f < n) || n == 0.
+            ))
+        });
+        (0..100).into_iter().map(|n: usize| n as f32).for_each(|n| {
+            println!("\nrand.next_float_signed({:?})...", n);
+            assert!(verify_rand(
+                || rand.next_float_signed(n),
+                |f| (-n <= f && f < n) || n == 0.
+            ))
+        });
     }
 
     #[test]
